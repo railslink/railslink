a59f6f08ecb87f8f66d9b112b870ace367029aac
Morio Okumura <morio.okumura@gmail.com>
2015-09-28 20:08:02 +0900

    Add timezone stats.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,24 @@
 class HomeController < ApplicationController
   
   def index
-    @users_list = SlackApiResponse.latest('users.list')
-    @valid_members = @users_list.response['members'].select{ |u| u['deleted'] == false }
-    @team_members_count = @valid_members.size
+    valid_members = SlackApiResponse.latest('users.list').response['members']
+      .select{ |u| u['deleted'] == false }
+    
+    @team_members_count = valid_members.size
     
     @channels = SlackApiResponse.latest('channels.list').response['channels']
       .sort_by{ |c| c['num_members'] }.reverse.first(6)
+      
+    # @timezones is a hash like 
+    # {
+    #   'UTC-7': 70,
+    #   'UTC-6': 54
+    # }
+    members_with_tz = valid_members.reject{ |m| m['tz'].nil? }
+    @timezones = (-11..14).map { |num| [
+      sprintf("UTC%+d", num), 
+      members_with_tz.count{ |m| (m['tz_offset'] / 3600) == num }
+    ] }.to_h
   end
   
 end
