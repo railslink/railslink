d96dba96c862cc2cdf34860f86f668ba87fecf80
Philip Hallstrom <philip@pjkh.com>
2018-05-15 22:11:26 -0700

    refactor slack event controller logic

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -5,14 +5,38 @@ class SlackController < ApplicationController
   # see https://api.slack.com/events-api
   # see https://api.slack.com/events/url_verification
   def event
-    render plain: params[:challenge]
+    if slack_params[:type] == "url_verification"
+      render plain: slack_params[:challenge]
+      return
+    end
+    head :ok
   end
 
   protected
 
   def verify_token!
-    return if params[:token] == ENV["SLACK_VERIFICATION_TOKEN"]
+    return if slack_params[:token] == ENV["SLACK_VERIFICATION_TOKEN"]
     head :forbidden
     return
   end
+
+  def slack_params
+    params.require(:slack).permit!
+  end
 end
+
+__END__
+"slack"=>{"token"=>"pL4lmrHYWTuJyCOEekfJfuw7",
+"team_id"=>"T05052K3Q",
+"api_app_id"=>"A9MR2EQDQ",
+"event"=>{"type"=>"message",
+"user"=>"U6D5W3NCC",
+"text"=>"something",
+"ts"=>"1526446776.000167",
+"channel"=>"C05052K88",
+"event_ts"=>"1526446776.000167",
+"channel_type"=>"channel"},
+"type"=>"event_callback",
+"event_id"=>"EvAQE3M1PD",
+"event_time"=>1526446776,
+"authed_users"=>["U054D8V3L"]}
--- spec/controllers/slack_controller_spec.rb
+++ spec/controllers/slack_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SlackController, type: :controller do
   describe "POST #event" do
     describe "when token is invalid" do
       it "returns http forbidden" do
-        post :event, params: {token: "invalid"}
+        post :event, params: {slack: {token: "invalid"}}
         expect(response).to have_http_status(:forbidden)
       end
     end
@@ -17,13 +17,22 @@ RSpec.describe SlackController, type: :controller do
       end
 
       it "returns http success" do
-        post :event, params: {token: token}
+        post :event, params: {slack: {token: token}}
         expect(response).to have_http_status(:success)
       end
 
-      it "renders the provided challenge" do
-        post :event, params: {token: token, challenge: "challenge"}
-        expect(response.body).to eq "challenge"
+      describe "type url_verification" do
+        it "renders the provided challenge" do
+          post :event, params: {slack: {token: token, type: "url_verification", challenge: "challenge"}}
+          expect(response.body).to eq "challenge"
+        end
+      end
+
+      describe "type unknown" do
+        it "renders nothing" do
+          post :event, params: {slack: {token: token, type: "unknown_invalid_etc", challenge: "challenge"}}
+          expect(response.body).to eq ""
+        end
       end
     end
   end
