af8d2f96fef08d5b597bf2a8d661b5da99a44974
Talha Mehmood <quiet.talha@gmail.com>
2018-10-13 17:18:41 +0500

    add hamburger

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,5 +31,12 @@
         logo.className += " spin";
       });
     });
+
+    var burger = document.querySelector('.navbar-burger');
+    var menu = document.querySelector('.navbar-menu');
+    burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+    });
   });
 }).call(this);
