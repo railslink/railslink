39791799f699e1ee29aca121cda1fa4f9a11624d
Kate Donaldson <kate@katelovescode.com>
2018-10-31 13:14:34 -0500

    Changes UsersController to inherit from AdminController since it shouldn't be accessible to non-admins, updates the show spec to create a factory and pass params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < AdminController
   def index
     @users = SlackUser.order(:is_deleted).order(is_admin: :desc).order(:last_name, :email)
   end
