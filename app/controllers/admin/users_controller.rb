2fac1483cf32888f9b75f35ae5465588ab16d9cc
Kate Donaldson <kate@katelovescode.com>
2018-10-31 18:37:53 -0500

    Adds User show page (#65)
    * Adds a route and view for users show page, adds View button to the users index
    
    * Changes UsersController to inherit from AdminController since it shouldn't be accessible to non-admins, updates the show spec to create a factory and pass params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,8 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < AdminController
   def index
     @users = SlackUser.order(:is_deleted).order(is_admin: :desc).order(:last_name, :email)
+  end
+  def show
+    @user = SlackUser.find(params[:id])
   end
 end
--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -4,7 +4,19 @@ RSpec.describe Admin::UsersController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
+      authorize_admin!
       get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      create(:slack_user)
+    end
+    it "returns http success" do
+      authorize_admin!
+      get :show, { params: {id: SlackUser.first.id } }
       expect(response).to have_http_status(:success)
     end
   end
