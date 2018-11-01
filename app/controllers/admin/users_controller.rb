6c667f1545db8c646dd4a6ce094c1cf98ac8ae79
Alex Piechowski <alex@piechowski.io>
2018-11-01 10:52:28 -0500

    Refactor Admin::UsersController SlackUser order chain (#69)
    Resolves #67

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 class Admin::UsersController < AdminController
   def index
-    @users = SlackUser.order(:is_deleted).order(is_admin: :desc).order(:last_name, :email)
+    @users = SlackUser.order(
+      :is_deleted,
+      { is_admin: :desc },
+      :last_name,
+      :email
+    )
   end
+
   def show
     @user = SlackUser.find(params[:id])
   end
