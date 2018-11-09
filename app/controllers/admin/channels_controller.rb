bd5883ccf86a0858a57893e24f8f2c0cf451b93d
Arun Kumar <arunmohandm@gmail.com>
2018-11-09 17:59:59 -0500

    Only let admins access /admin/channels (#79)

--- app/controllers/admin/channels_controller.rb
+++ app/controllers/admin/channels_controller.rb
@@ -1,4 +1,4 @@
-class Admin::ChannelsController < ApplicationController
+class Admin::ChannelsController < AdminController
   def index
     @channels = SlackChannel.order(:is_archived, :name)
   end
--- spec/controllers/admin/channels_controller_spec.rb
+++ spec/controllers/admin/channels_controller_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe Admin::ChannelsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context "guest user" do
+      it "redirects to /auth/slack" do
+        get :index
+        expect(response).to redirect_to("/auth/slack?require=admin")
+      end
+    end
+
+    context "admin user" do
+      it "returns http success" do
+        authorize_admin!
+        get :index
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
