d6e141fa4229a7f94003bb8245b55e14eb706528
Philip Hallstrom <philip@pjkh.com>
2019-03-23 13:46:48 -0700

    add code of conduct
    fixes #83
    
    This patch puts the CoC in a markdown file in the root of the repository
    and adds a rails route to load it in, convert it to HTML, and display it.
    
    It also puts a link to the CoC in the footer, replacing twitter which we
    already had an icon link to.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def admin_members
     @admin_members = SlackUser.admins.active.sort_by_recent_activity
   end
+
+  def code_of_conduct
+    text = File.read(File.join(Rails.root, "CODE_OF_CONDUCT.md"))
+    @coc_html = Kramdown::Document.new(text).to_html
+  end
 end
