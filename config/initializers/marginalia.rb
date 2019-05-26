b237668927d5b682a3d381c2c8b8518e4bb3fdbb
Philip Hallstrom <philip@pjkh.com>
2019-05-26 09:42:01 -0700

    tweak marginalia config so it ignores ruby's monitor.rb stack frame
    Marginalia and Ruby 2.4/2.5 have an issue where the stack trace includes
    Ruby's own monitor.rb and this isn't excluded by the default
    configuration so it shows all SQL queries as coming from there. This
    patch adds it to the exclusion list.
    
    See https://github.com/basecamp/marginalia/issues/70
    
    test plan:
      - fire up rails server and watch the logs
      - load up the homepage
      - notice that the SQL comments appropriately show where they are
        invoked instead of pointing to monitor.rb

--- config/initializers/marginalia.rb
+++ config/initializers/marginalia.rb
@@ -1,2 +1,6 @@
 # see https://github.com/basecamp/marginalia#customization
 Marginalia::Comment.components = [:controller_with_namespace, :action, :line]
+
+# See https://github.com/basecamp/marginalia/blob/master/lib/marginalia/comment.rb#L109
+# Our addition excludes:  "/usr/local/lib/ruby/.../monitor.rb:...:in `mon_synchronize'"
+Marginalia::Comment.lines_to_ignore = /\.rvm|gem|vendor\/|marginalia|rbenv|\/monitor.rb.*mon_synchronize/
