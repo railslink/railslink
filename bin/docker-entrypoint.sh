2265b378dd506859871c10db8c2d8cea577bd73b
Owajigbanam Ogbuluijah <igbanam@andela.com>
2019-10-16 12:51:48 +0100

    No need to remove the server.pid

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# remove the server.pid
-if [ -f $APP_DIR/tmp/pids/server.pid ]; then
-  echo "---- REMOVING pid from $APP_DIR ----"
-  rm $APP_DIR/tmp/pids/server.pid
-fi
-
 bundle check || bundle install
 
 exec /sbin/tini -- "$@"
