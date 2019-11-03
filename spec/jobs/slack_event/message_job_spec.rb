acc12ec2fc1297cc0df7e262300b9bdeb71a2e94
Philip Hallstrom <philip@pjkh.com>
2019-11-03 11:37:50 -0800

    DM user's who use /invite_people to use web form
    Awhile ago Slack made a change so that anyone can use /invite_people to
    invite anyone to the Slack team. We prefer that people use the signup
    form so we can review them before approval.
    
    We've configured the Slack instance to send these messages to
    `#admin-invitations`, but we still have to then DM the requestor and ask
    them to point their invitee to the signup form.
    
    This patch extends the MessageJob slack event job to watch for these
    messages in `#admin-invitations`, and DM the requestor on our behalf.

--- spec/jobs/slack_event/message_job_spec.rb
+++ spec/jobs/slack_event/message_job_spec.rb
@@ -1,12 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe SlackEvent::MessageJob, type: :job do
+  describe "#request_to_invite" do
+    context "when not an invitation request" do
+      it "won't invoke when message text is wrong" do
+        expect(subject).to receive(:request_to_invite).and_return(false)
+        payload = { event: { text: "not an invitation request" } }
+        subject.perform(payload)
+      end
+
+      it "won't invoke when fallback text is wrong" do
+        expect(subject).to receive(:request_to_invite).and_return(false)
+        payload = { event: {
+          text: "<@UQ5TPSAKG> requested to invite one person to this workspace.",
+          attachments: [{fallback: "not an approve or deny fallback"}]
+        } }
+        subject.perform(payload)
+      end
+    end
+
+    context "when an invitation request" do
+      let(:user_id) { "UQ5TPSAKG" }
+      let(:payload) {{
+        event: {
+          text: "<@#{user_id}> requested to invite one person to this workspace.",
+          attachments: [{fallback: "Approve/Deny the invite request on team site"}]
+        }
+      }}
+
+      it "sends a dm to the user" do
+        client = double(Slack::Web::Client)
+        im = double(channel: double(id: 123))
+        expect(Slack::Web::Client).to receive(:new).and_return(client)
+        expect(client).to receive(:im_open).with(user: user_id).and_return(im)
+        expect(client).to receive(:chat_postMessage).with(hash_including(
+          channel: 123,
+          text: kind_of(String)
+        ))
+        subject.perform(payload)
+      end
+    end
+  end
+
   describe "#update_last_message_at" do
     it "updates the user's last_message_at attribute" do
       user = create(:slack_user)
       event_ts = Time.current
       subject.perform( event: { user: user.uid, event_ts: event_ts.to_f.to_s })
       expect(user.reload.last_message_at.to_i).to eq event_ts.to_i
+    end
+
+    it "won't invoke if a previous handler processes the message" do
+      allow(subject).to receive(:request_to_invite).and_return(true)
+      subject.perform({})
+      expect(subject).not_to receive(:update_last_message_at)
     end
   end
 end
