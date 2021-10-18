2b7db40b46a8db8780bf36c623ca11d547faaa7f
Philip Hallstrom <philip@pjkh.com>
2021-10-18 11:33:26 -0700

    Update invocation to work with Rails 6.
    In Rails 5, ActionView::Base.new took three arguments each defaulting to
    nil. In Rails 6 they do not default -- however, they introduced `empty`
    which does the same thing (passing nils into new).

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -3,12 +3,12 @@
 class BasePresenter < SimpleDelegator
   attr_reader :view
 
-  def initialize(model, view = ActionView::Base.new)
+  def initialize(model, view = ActionView::Base.empty)
     @view = view
     super(model)
   end
 
-  def self.from_collection(collection, view = ActionView::Base.new)
+  def self.from_collection(collection, view = ActionView::Base.empty)
     collection.map { |model| new(model, view) }
   end
 
