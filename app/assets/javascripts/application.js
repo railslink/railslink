f06fda99462143005be6ba809369af6d4242b65c
Philip Hallstrom <philip@pjkh.com>
2021-10-18 11:51:33 -0700

    eliminate annoying console warning on homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,10 +31,12 @@
     });
 
     var burger = document.querySelector('.navbar-burger');
-    var menu = document.querySelector('.navbar-menu');
-    burger.addEventListener('click', function() {
-      burger.classList.toggle('is-active');
-      menu.classList.toggle('is-active');
-    });
+    if ( burger ) {
+      var menu = document.querySelector('.navbar-menu');
+      burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+      });
+    }
   });
 }).call(this);
